53a62f1a4a60decb7bcf782d58f0db13342d7536
Eitan Knopp <eknopp@Eitans-MacBook-Air.local>
2023-03-08 15:58:07 +0200

    CORS error fix test

--- server.js
+++ server.js
@@ -19,9 +19,15 @@ const db = knex({
     }
   });
 
+  
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next();
+}) 
 
 app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt)) 

ca3f7523bbc1c0a995c3ca17099e182e36450f72
Eitan Knopp <eknopp@Eitans-MacBook-Air.local>
2023-03-08 16:40:30 +0200

    CORS error fix test 5

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const db = knex({
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+const corsOptions = {
+  origin: "http://localhost:3000/",
+};
+
+app.use(cors(corsOptions));
 
 
 app.get('/', (req, res) => { res.send('it is working') })

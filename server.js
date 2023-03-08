9337764fa3c5766f185c07bff73d826fe2b863ff
Eitan Knopp <eknopp@Eitans-MacBook-Air.local>
2023-03-08 16:15:23 +0200

    CORS error fix test 2

--- server.js
+++ server.js
@@ -24,10 +24,11 @@ const db = knex({
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-}) 
+})
 
 app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt)) 
